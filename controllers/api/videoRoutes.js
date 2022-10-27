d62bd28afda443cc8815d58267df8db0e769355f
Nordleen De Frias <nordleendefrias@gmail.com>
2022-10-27 10:43:16 -0400

    Adding branch

---
+++ controllers/api/videoRoutes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const {Video} = require('../../models');
+const {Post} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// EXTRACT VIDEO - from end-user post - automatic
+// Given a recent post's URL field is true
+// Then extract URL and place it in the video page
+// proceed to update the video list
+// then add to top of the list
+router.get('/', async (req, res) => {
+    const recentPost = await Post.finAll ({
+        include: [
+            {
+                model: Post,
+                attributes: ['url_link'],
+            },
+        ],
+    });
+    console.log(recentPost);
+
+    const thePost = postData.map((post) => thePost.get({plain: true}));  // Remove garbage from serialized object
+    console.log(thePost);
+
+    res.render('videos', {
+        thePost: thePost,
+        //login not required
+    });
+});
+
+// POST VIDEO by end user - NOT automatic
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newVideo = await Video.create({
+      ...req.body,
+      user_id: req.session.post_id,
+    });
+
+    res.status(200).json(newVideo);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE VIDEO
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const videoData = await Video.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.post_id,
+      },
+    });
+
+    if (!videoData) {
+      res.status(404).json({message: 'No video found with this id!'});
+      return;
+    }
+
+    res.status(200).json(videoData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const {User, Post, Video} = require('../models');
+
+// given a user lands on our page
+// when they page loads
+// then they are show a list of latest post extracted from our db
+// HOME PAGE ROUTE - user lands here
+router.get('/', async (req, res) => {
+  try {  // try looking for latest post
+    const postData = await Post.findAll(); // filter that down w/ the config to get the ones you want
+    console.log(postData) // sanity check
+    const latestPosts = postData.map(function(post) { // clean post data
+      return post.get({plain: true});  // <-- this is the object that cleans up the data from the sequilaze object garbage
+    });
+    console.log(latestPosts);
+    res.json(latestPosts);  // sanity check
+
+    req.render('homepage'); // render a page and send post data
+  } catch (err) {
+    res.status(500).json(err);  
+  }
+});
+
+// GIVEN a user clicks and is not loged-in, 
+// THEN they are redirected to the login page.
+// DISCUSSION-POSTS redirect
+// Prevent non-logged users from viewing discussion post page
+router.get('/postRoutes', withAuth, async (req, res) => {
+    try { 
+      const users = userData.map((project) => project.get({plain: true}));
+      res.render('discussion-posts', {
+        users,
+        logged_in: req.session.logged_in,   // Pass the logged in flag to the template
+      });
+    } catch (err) {
+      res.status(401).json(err);  //Unauthorized error specifically for use when authentication is required
+    }
+  });
+
+//If a session exits
+//redirect the request to the Discussion Post Page
+router.get('/login', (req, res) => {
+    // If a session exists, redirect the request to the Discussion Post Page
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+    res.render('login');
+  });
+  
+module.exports = router;
