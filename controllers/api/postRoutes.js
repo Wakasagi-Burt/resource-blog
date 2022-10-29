9918943e1ef17345e653a20b35b60ee742f50032
Nordleen De Frias <nordleendefrias@gmail.com>
2022-10-29 18:46:22 -0400

    Pushing work on Discussion post and login

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
-const {Post} = require('../../models');
+const {Post} = require('../../models/Post');
 const withAuth = require('../../utils/auth');
 
-// Create a new post route
+// Create a new Blog Post
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newPost = await Post.create({     // new post object
-      ...req.body,                          // read input
-      user_id: req.session.user_id,         // require user ID
+    const newPost = await Post.create({     // new postBlog object
+      title: req.body.title,
+      postdate: req.body.postdate,  // <-- we need to write a function on js file to insert the auto date
+      content: req.body.content,
+      username: req.body.username,          // read input
+      user_id: req.session.user_id,         // require user ID to post on blog
     });
 
     res.status(200).json(newPost);          // successful post
@@ -16,24 +19,25 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-// Delete route
-router.delete('/:id', withAuth, async (req, res) => {
+//Sending the data to the model and view
+router.put('/:id', async (req, res) => {  //makes promise
   try {
-    const postData = await Post.destroy({  // Destroy becasue deleting
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
+    const post = await Post.update (  // Wait until result is resolved to post
+      {
+        title: req.body.title,        // Creating object of Blog Post
+        postdate: req.body.postdate,
+        content: req.body.postdate,
+        username: req.body.username,
       },
-    });
-
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
+      {
+        where: {                      // Where there's a required ID
+          id: req.params.id,
+        },
+      }
+    );  
+    req.status(200).json(post);  
+  } catch (error) {
+    res.status(500).json(500);
   }
 });
 
