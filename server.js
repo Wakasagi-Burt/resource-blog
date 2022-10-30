ff352ac388b11067d5386d00feb7954dd8b5f91e
hiren patel <hirenpatel0526@yahoo.com>
2022-10-29 18:58:12 -0700

    sdfa

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
 const sess = {
-  secret: 'Tech blog secret',
+  secret: 'resource blog',
   cookie: {},
   resave: false,
   saveUnintialized: true,
