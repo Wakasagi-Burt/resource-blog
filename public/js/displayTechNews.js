41c4ee91a6bd0f870173c4683d90ed8bd94bbfbd
ALAN JOSE <alanjose04231120@gmail.com>
2022-10-30 22:04:10 -0400

    created final changes for api

--- public/js/displayTechNews.js
+++ public/js/displayTechNews.js
@@ -1,48 +1,46 @@
-// const displaytech = async () => {
-//     const response = await fetch('/', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+const displaytech = async () => {
+    const response = await fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
   
-//     if (response.ok) {
-//         let newtechnews = new Object();          
-//            fetch('https://hacker-news.firebaseio.com/v0/beststories/.json?print=pretty', {
-//             method: 'GET', //GET is the default.
-//              // include, *same-origin, omit
-//             redirect: 'follow', // manual, *follow, error
-//           })
-//             .then(function (response) {
-//               return response.json();
-//             })
-//             .then(function (data) {
-//               for(i = 0; i < 5; i++) {
-//                 let id = data[i];
-//                 fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`, {
-//                   method: 'GET', //GET is the default.
-//                    // include, *same-origin, omit
-//                   redirect: 'follow', // manual, *follow, error
-//                 })
-//                   .then(function (response) {
-//                     return response.json();
-//                   })
-//                   .then(function (data) {
-                    
-//                     // let head = data['title'];
-//                     // let author = data['by'];
-//                     // let datapoint = data['url'];
-//                     // newtechnews = {head,author,datapoint};
-//                     newtechnews.head = data.title;
-//                     newtechnews.author = data.by;
-//                     newtechnews.datapoint = data.url;
-//                     arrayoftech.push(newtechnews);
-                
-                
-//               })
-//         }})}
-     
-//     else {
-//       alert(response.statusText);
-//     }
-//   };
+    if (response.ok) {
+let newtechnews = new Object();
+const technews = () => {
+  
+   fetch('https://hacker-news.firebaseio.com/v0/beststories/.json?print=pretty', {
+    method: 'GET', //GET is the default.
+     // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+  })
+    .then((response) => response.json())
+    .then(function (data) {
+      for(i = 0; i < 5; i++) {
+        let id = data[i];
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`, {
+          method: 'GET', //GET is the default.
+           // include, *same-origin, omit
+          redirect: 'follow', // manual, *follow, error
+        })
+          .then((response) => response.json())
+          
+          .then(function (data) {
+            
+            let head = data['title'];
+            let author = data['by'];
+            let datapoint = data['url'];
+            newtechnews = {head,author,datapoint};
+        
+      })
+
+      // arrayoftech[i] = newtechnews;  
+      // console.log(arrayoftech);
+}})}
+technews();
+}
+else {
+      alert(response.statusText);
+    }
+  };
   
 
